0e87327f2099c0cfb5c3970e92d590fed5e4d139
AhmedMamdou798 <you@example.com>
2024-09-15 00:45:21 +0300

    task 1 is done

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 5;
+number2 = 10;
+sum_result = number1 + number2;
+Subtraction_result = number2 - number1;
+Multiplication_result = number1 * number2;
+
+print(f"Addition of {number1} and {number2} is {sum_result}");
+print(f"Subtraction of {number1} and {number2} is {Subtraction_result}");
+print(f"Multiplication of {number1} and {number2} is {Multiplication_result}");
