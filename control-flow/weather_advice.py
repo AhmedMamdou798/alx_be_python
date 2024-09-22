04f6a2b5efee894ca38eaa9debd6ae3db6b900ea
AhmedMamdou798 <you@example.com>
2024-09-22 21:51:17 +0300

    code correction

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -2,6 +2,8 @@ weather_state = input("What's the weather like today? (sunny/rainy/cold):");
 if weather_state == "sunny" :
     print("Wear a t-shirt and sunglasses.");
 elif weather_state == "rainy" :
+    print("Don't forget your umbrella and a raincoat.");
+elif weather_state == "cold" :
     print("Make sure to wear a warm coat and a scarf.");
 else :
     print("Sorry, I don't have recommendations for this weather")
