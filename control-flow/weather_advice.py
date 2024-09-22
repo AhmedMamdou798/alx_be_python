562fa6f136edc79ace235c32da7563089008d3b2
AhmedMamdou798 <you@example.com>
2024-09-22 21:42:45 +0300

    Weather Recommendation Program

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,7 @@
+weather_state = input("What's the weather like today? (sunny/rainy/cold):");
+if weather_state == "sunny" :
+    print("Wear a t-shirt and sunglasses.");
+elif weather_state == "rainy" :
+    print("Make sure to wear a warm coat and a scarf.");
+else :
+    print("Sorry, I don't have recommendations for this weather")
